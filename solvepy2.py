164885691cfb1f1748fafb9affbb9c9c2c09bedb
ssumin6 <ssumin6@gmail.com>
2019-05-20 21:35:27 +0900

    wanted to design cdcl method

--- solvepy2.py
+++ solvepy2.py
@@ -2,6 +2,9 @@ import sys
 import copy
 import random
 
+assigned = []
+learned = []
+
 def clause_strip(clause):
     #used in parsing the DIMACS format input
     ret = []
@@ -16,66 +19,132 @@ def clause_strip(clause):
             tmp.append(int(i))
     return ret
 
-def containsUnit(clauses, num):
+def containsUnit(clauses, num, answer):
     for i in range(len(clauses)):
-        if (len(clauses[i]) == num):
+        count = 0
+        for j in clauses[i]:
+            if j in answer:
+                break
+            if -1*j not in answer:
+                count += 1
+        if count == num:
             return i
     return -1
 
+def isClauseEmpty(clauses, answer):
+    if len(clauses)== 0:
+        return True
+    for i in clauses:
+        for j in i:
+            if j in answer:
+                break
+            if -1*j not in answer:
+                return False
+    return True
+    
+
 def unitPropa(clauses, v):
     i = 0
-    nv = -1*v
     while (i < len(clauses)):
         if v in clauses[i]:
-            del clauses[i]
+            clauses.pop(i)
             continue
-        elif nv in clauses[i]:
-            clauses[i].remove(nv)
         i += 1
     return clauses
 
-def DPLL(clauses, answer):
-    #print("THIS IS C")
-    #print(clauses)
+def resolution(a1, a2):
+    for i in a1:
+        if -1*i in a2:
+            a1.remove(i)
+            a2.remove(-1*i)
+            a1.extend(a2)
+            a1 = list(set(a1))
+            return a1
+    return a1.extend(a2)
 
-    i = containsUnit(clauses, 1)
+def conflict(graph_e, c_clause):
+    global assigned
+    global learned
+
+    tmp = []
+    #backtracking
+    for i in tmp:
+        if -1*i not in assigned:
+            continue
+        j = len(assigned)-1
+        while (j >= 0):
+            x = assigned.pop()
+            if x == -1*i: 
+                break
+
+    #add the learned clause
+    if len(tmp) != 0:
+        learned.append(tmp)
+
+def DPLL(clauses, answer, graph_e):
+    global assigned
+    global learned
+
+    clauses.extend(learned)
+
+    i = containsUnit(clauses, 1, answer)
+    #print(clauses)
     while (i>=0):
         #Unit propagation
         v = clauses[i][0]
+        for j in clauses[i]:
+            if -1*j not in answer and j not in answer:
+                v = j
+                break
         answer.append(v)
-        del clauses[i]
+
+        for j in clauses[i]:
+            if j != v:
+                graph_e.append((-1*j, v))
+    
+        clauses.pop(i)
         clauses = unitPropa(clauses, v)
-        i = containsUnit(clauses, 1)
+        i = containsUnit(clauses, 1, answer)
 
-    #print("THIS IS A")
-    #print(answer)
-
-    if (len(clauses) == 0):
+    if (isClauseEmpty(clauses, answer)):
         return answer
     
-    if (containsUnit(clauses, 0) >= 0):
-        #learned clause 
-        #print("CONFLICT CASE")
+    c_index = containsUnit(clauses, 0, answer)
+    if (c_index >= 0):
+        print("conflict")
+        conflict(graph_e, clauses[c_index])
         return False
    
     backup1 = copy.deepcopy(clauses)
     backup2 = copy.deepcopy(clauses)
     ans1 = copy.deepcopy(answer)
     ans2 = copy.deepcopy(answer)
-
+    e1 = copy.deepcopy(graph_e)
+    
     l = random.randrange(1, nVar)
     while (l in answer) or (-1*l in answer):
         l = random.randrange(1, nVar)
 
-    v = DPLL(backup1 + [[l]], ans1)
-
+    assigned.append(l)
+    v = DPLL(backup1 + [[l]], ans1, e1)
     if v:
-        print("INSIDE ")
         return v
-    v = DPLL(backup2 + [[-1*l]], ans2)
+    
+    if l in assigned:
+        assigned.remove(l)
+        assigned.append(-1*l)
+    else:
+        return False
+
+    e2 = copy.deepcopy(graph_e)
+
+    v = DPLL(backup2 + [[-1*l]], ans2, e2)
     if v: 
         return v
     
+    if -1*l in assigned:
+        assigned.remove(-1*l)
+
     return False
     
 
@@ -102,9 +171,10 @@ while line:
         break 
 
     line = f.readline()
+f.close()
 
 clauses = clause_strip(clauses)
-ans = DPLL(clauses, [])
+ans = DPLL(clauses, [], [])
 
 #print the answer in DIMACS Format
 if (ans):
@@ -113,5 +183,3 @@ if (ans):
     print("v "+ " ".join(map(str,ans)))
 else:
     print("s UNSATISFIABLE")
-f.close()
-
